997f5cac1ac7a7e839deb3d8180a3ef64f618781
FL03 <jo3mccain@icloud.com>
2023-05-22 22:13:40 -0500

    update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- app/src/cmp/sidebar.rs
+++ app/src/cmp/sidebar.rs
@@ -5,17 +5,19 @@
 #![allow(non_snake_case)]
 use dioxus::prelude::*;
 
-#[derive(Clone, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd, Props)]
-pub struct SidebarProps {
-    title: String
+#[derive(Clone, Debug, Default, Props)]
+pub struct SidebarProps<'a> {
+    pub children: Element<'a>,
+    pub class: &'a str,
+    pub title: String
 }
 
 /// [Sidebar] is a dynamic component typically leveraged with data-driven views
-pub fn Sidebar(cx: Scope<SidebarProps>) -> Element {
+pub fn Sidebar<'a>(cx: Scope<'a, SidebarProps<'a>>) -> Element {
     cx.render(rsx! {
         div { class: "flex flex-col items-center justify-center min-h-screen bg-gray-900", id: "sidebar",
             div {class: "", id: "sidebar-header",
-                h1 { class: "text-4xl text-white font-bold", "Sidebar" }
+                h1 { class: "text-4xl text-white font-bold", "{cx.props.title}" }
             }
         }
     })
