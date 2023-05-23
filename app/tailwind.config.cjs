d23deb65245ac9ea98d62abbb74b077ef9a5f680
FL03 <jo3mccain@icloud.com>
2023-05-22 19:48:41 -0500

    update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -1,13 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 const config = {
     content: [
-        "./dist/**/*.{css,html}",
+        "./dist/**/*.{css,html,js}",
         "./src/**/*.{css,html,rs}", 
         "./static/**/*.{css,html}"
     ],
     darkMode: 'class', // or 'media' or 'class'
     mode: 'jit',
-    plugins: [],
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+        require('flowbite/plugin')
+    ],
     theme: {
         extend: {},
     },
